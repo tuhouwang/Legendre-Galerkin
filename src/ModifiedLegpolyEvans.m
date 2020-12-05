acac04bd87377441fa70e27f928974b73a133d54
Houwang Tu <tuhouwang96@163.com>
2020-12-05 13:30:15 +0800

    修改一处N+1-->N-1

--- src/ModifiedLegpolyEvans.m
+++ src/ModifiedLegpolyEvans.m
@@ -3,7 +3,7 @@ function [L,Lp] = ModifiedLegpolyEvans(N, x)
     [L,Lp] = legpoly(N, x);
     
     if(mod(N, 2) == 0)
-        for k = 1 : N+1
+        for k = 1 : N-1
             if(mod(k,2) == 1)
                 L (:, k) = L (:, k) - L (:, N+1);
                 Lp(:, k) = Lp(:, k) - Lp(:, N+1);
@@ -13,7 +13,7 @@ function [L,Lp] = ModifiedLegpolyEvans(N, x)
             end
         end
     else
-        for k = 1 : N+1
+        for k = 1 : N-1
             if(mod(k, 2) == 1)
                 L (:, k) = L (:, k) - L (:, N);
                 Lp(:, k) = Lp(:, k) - Lp(:, N);
